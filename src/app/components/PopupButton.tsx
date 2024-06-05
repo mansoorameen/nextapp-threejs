2f26990df7200e1a3c616727b496e66f11efb637
mansoor <manzooravail@gmail.com>
2024-06-06 01:03:39 +0530

    update: convert popup functionality to server components

--- src/app/components/PopupButton.tsx
+++ src/app/components/PopupButton.tsx
@@ -1,39 +1,25 @@
-'use client';
+import { headers } from "next/headers";
+import Link from "next/link";
 
-import { ReactNode, useState } from 'react';
+const PopupButton: React.FC = () => {
+  const headersList = headers();
+  const pathname = headersList.get("x-pathname");
 
-interface PopupButtonProps {
-  children: ReactNode;
-}
+  const url = new URL(
+    pathname ? pathname : "/",
+    process.env.NEXT_PUBLIC_DOMAIN_NAME || "http://localhost:3001"
+  );
+  url.searchParams.set("modal", "true");
 
-const PopupButton: React.FC<PopupButtonProps> = ({children}) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-
-  const handleOpenPopup = (): void  => {
-    setIsOpen(true);
-  };
-
-  const handleClosePopup = (): void => {
-    setIsOpen(false);
-  };
-
-    return (
-    <div>
-      <button onClick={handleOpenPopup} className="px-4 py-2 bg-blue-500 text-white rounded">
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Link
+        href={url.toString()}
+        scroll={false}
+        className="px-4 py-2 bg-blue-500 text-white rounded"
+      >
         Open Popup
-      </button>
-
-      {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="relative p-8 bg-white rounded shadow-lg">
-            <button onClick={handleClosePopup} className="absolute top-2 right-2 px-2 py-1 bg-red-500 text-white rounded">
-              Close
-            </button>
-            {children}
-          </div>
-        </div>
-      )}
+      </Link>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,44 @@
-import Link from 'next/link';
-import PopupWrapper from './components/PopupWrapper'
+import Link from "next/link";
+import PopupButton from "./components/PopupButton";
+import PopupContent from "./components/PopupContent";
+import { Suspense } from "react";
+import Loading from "./components/Loading";
 
-const Home: React.FC = () => {
+export type Props = {
+  searchParams: Record<string, string> | null | undefined;
+};
+
+const Home: React.FC<Props> = (props) => {
+  const { searchParams } = props;
+  const showModal = searchParams?.modal === "true";
+
   return (
     <>
-    <div className="flex gap-5 m-2">
-    <Link href='/' className="border border-black p-2 bg-red-100">Home</Link>
-    <Link href='/animation' className="border border-black p-2">Animation</Link>
-    </div>
+      <div className="flex gap-5 m-2">
+        <Link href="/" className="border border-black p-2 bg-red-100">
+          Home
+        </Link>
+        <Link href="/animation" className="border border-black p-2">
+          Animation
+        </Link>
+      </div>
 
-    {/* Popup */}
-    <div className="flex items-center justify-center h-screen">
-      <PopupWrapper />
-    </div>
+      <PopupButton />
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="relative p-8 bg-white rounded shadow-lg">
+            <Link
+              href="/"
+              className="absolute top-2 right-2 px-2 py-1 bg-red-500 text-white rounded"
+            >
+              Close
+            </Link>
+            <Suspense fallback={<Loading />}>
+              <PopupContent />
+            </Suspense>
+          </div>
+        </div>
+      )}
     </>
   );
 };
